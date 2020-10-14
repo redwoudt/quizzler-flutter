4976c733bf0cff98681cc518da96cd2288906944
Ferdi Redelinghuys <ferdi.redelinghuys@vulamobile.com>
2020-10-14 15:08:49 +0200

    added class Question

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,10 +29,12 @@ class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
   int questionNumber = 0;
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true)
   ];
 
   void nextQuestionNumber() {
@@ -39,6 +42,14 @@ class _QuizPageState extends State<QuizPage> {
     if (questionNumber >= questions.length) {
       questionNumber = 0;
     }
+  }
+
+  void addCorrectIcon() {
+    print('Correct Answer!');
+  }
+
+  void addWrongIcon() {
+    print('Wrong Answer!');
   }
 
   @override
@@ -53,7 +64,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,6 +89,11 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  if (questions[questionNumber].questionAnswer == false) {
+                    addCorrectIcon();
+                  } else {
+                    addWrongIcon();
+                  }
                   nextQuestionNumber();
                 });
               },
@@ -98,6 +114,11 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  if (questions[questionNumber].questionAnswer == false) {
+                    addCorrectIcon();
+                  } else {
+                    addWrongIcon();
+                  }
                   nextQuestionNumber();
                 });
                 //The user picked false.
