839ca48003a1456dd1f48e70c3a8e609db6642ac
Ferdi Redelinghuys <ferdi.redelinghuys@vulamobile.com>
2020-10-15 11:06:47 +0200

    added QuizBrain Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -29,17 +31,10 @@ class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
   int questionNumber = 0;
-  List<Question> questions = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true)
-  ];
 
   void nextQuestionNumber() {
     questionNumber++;
-    if (questionNumber >= questions.length) {
+    if (questionNumber >= quizBrain.questions.length) {
       questionNumber = 0;
     }
   }
@@ -64,7 +59,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber].questionText,
+                quizBrain.questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -89,7 +84,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  if (questions[questionNumber].questionAnswer == false) {
+                  if (quizBrain.questions[questionNumber].questionAnswer ==
+                      false) {
                     addCorrectIcon();
                   } else {
                     addWrongIcon();
@@ -114,7 +110,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  if (questions[questionNumber].questionAnswer == false) {
+                  if (quizBrain.questions[questionNumber].questionAnswer ==
+                      false) {
                     addCorrectIcon();
                   } else {
                     addWrongIcon();
@@ -133,9 +130,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
